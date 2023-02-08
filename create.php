db55b0abbfb81131b70efe1c4319c368fdacddf5
Renan Aragao <renan.aragao159@gmail.com>
2023-02-08 20:04:36 -0300

    Criando templates

--- create.php
+++ create.php
@@ -1,7 +1,7 @@
 <?php
     include_once("templates/header.php");
 ?>
-    <h1>Testanto Agendaa</h1>
+    <h1>Criando contato</h1>
 <?php
     include_once("templates/footer.php");
 ?>
---
+++ templates/header.php
@@ -0,0 +1,32 @@
+<?php
+    include_once("config/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de Contatos</title>
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- FONT AWESOME-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
+            <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
+        </a>
+        <div>
+            <div class="navbar-nav">
+                <a class="nav-link" id="home-link" href="<?= $BASE_URL ?>index.php" >Agenda</a>
+                <a class="nav-link" id="home-link" href="<?= $BASE_URL ?>create.php" >Adicionar Contato</a>
+            </div>
+        </div>
+    </nav>
+</header>
