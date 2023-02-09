73eece69eee6bb243d55e4079a3f1468a737e1df
Renan Aragao <renan.aragao159@gmail.com>
2023-02-08 21:52:29 -0300

    Tela de registro de contato

--- config/process.php
+++ config/process.php
@@ -4,9 +4,24 @@
     include_once("connection.php");
     include_once("url.php");
     
-    $contacts = [];
-    $query = "SELECT * FROM contacts";
-    $stmt = $conn->prepare($query);
-    $stmt->execute();
-    $contacts = $stmt->fetchAll();
+    if(!empty($_GET)) {
+        $id = $_GET["id"];
+    }
+    
+    // Retorna os dados de um contato
+    if(!empty($id)) {
+        $query = "SELECT * FROM contacts WHERE id = :id";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        $contact = $stmt->fetch();
+
+    } else {
+        // Retorna todos os contatos
+        $contacts = [];
+        $query = "SELECT * FROM contacts";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $contacts = $stmt->fetchAll();
+    }
 ?>
