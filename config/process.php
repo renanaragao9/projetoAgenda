8138e72c257c39a1512a5c368c576fd421bbef63
Renan Aragao <renan.aragao159@gmail.com>
2023-02-08 21:05:47 -0300

    Criando a pagina inicial e trazendo os dados

--- config/process.php
+++ config/process.php
@@ -3,7 +3,8 @@
 
     include_once("connection.php");
     include_once("url.php");
-
+    
+    $contacts = [];
     $query = "SELECT * FROM contacts";
     $stmt = $conn->prepare($query);
     $stmt->execute();
