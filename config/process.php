89c4d8b1f5fda2d3e4517653c492cf4ec23a7718
Renan Aragao <renan.aragao159@gmail.com>
2023-02-09 21:47:03 -0300

    Removendo contatos

--- config/process.php
+++ config/process.php
@@ -49,6 +49,20 @@
                 $error = $e->getMessage();
                 echo "Erro: $error";
             }
+        } else if($data["type"] === "delete") {
+            $id = $data["id"];
+            $query = "DELETE FROM contacts WHERE id = :id";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(":id", $id);
+
+            try {
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato removido com sucesso!";
+            } catch(PDOException $e) {
+                // erro na conexão
+                $error = $e->getMessage();
+                echo "Erro: $error";
+            }
         }
 
         //  REDIRECT HOME
